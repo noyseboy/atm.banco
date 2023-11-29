a7f0399b3b7a6d1da9fa88f3b5e3cf755f722f29
thyago silva <131825997+noyseboy@users.noreply.github.com>
2023-11-29 16:02:26 -0300

    alteração condiçao nas variaveis

--- atm_banco.py
+++ atm_banco.py
@@ -12,9 +12,12 @@ class ATM:
         print("3. Sair")
 
     def login(self):
-        # Método para permitir que um usuário faça login em sua conta
-        numero_conta = input("Digite o número da sua conta: ")
-        senha = input("Digite sua senha: ")
+        numero_conta = input("Digite o número da sua conta: ").strip()
+        senha = input("Digite sua senha: ").strip()
+
+        if not numero_conta or not senha or not numero_conta.isdigit():
+            print("Por favor, preencha todos os campos com números.")
+            return
 
         for conta in self.contas:
             if conta['numero_conta'] == numero_conta and conta['senha'] == senha:
@@ -24,10 +27,13 @@ class ATM:
             print("Conta não encontrada. Verifique o número da conta e a senha.")
 
     def cadastrar_conta(self):
-        # Método para permitir que um novo usuário cadastre uma nova conta
-        nome = input("Digite seu nome: ")
-        cpf = input("Digite seu CPF: ")
-        senha = input("Digite sua senha: ")
+        nome = input("Digite seu nome: ").strip()
+        cpf = input("Digite seu CPF: ").strip()
+        senha = input("Digite sua senha: ").strip()
+
+        if not nome or not cpf or not senha or not cpf.isdigit():
+            print("Por favor, preencha todos os campos corretamente.")
+            return
 
         for conta in self.contas:
             if conta['cpf'] == cpf:
@@ -43,11 +49,9 @@ class ATM:
         self.menu_conta(nova_conta)
 
     def gerar_numero_conta(self):
-        # Método para gerar um número de conta aleatório
         return str(random.randint(100000, 999999))
 
     def menu_conta(self, conta):
-        # Método para exibir o menu principal da conta e processar as escolhas do usuário
         while True:
             print("\nBem-vindo, {}!".format(conta['nome']))
             print("Número da conta:", conta['numero_conta'])
@@ -71,18 +75,23 @@ class ATM:
             elif escolha == '5':
                 self.imprimir_relatorio(conta)
             elif escolha == '6':
-                print("Obrigado por usar nossos serviços. Até logo!")
+                print("Obrigado por usar nosso ATM. Até logo!")
                 break
             else:
                 print("Opção inválida. Tente novamente.")
 
     def verificar_saldo(self, conta):
-        # Método para exibir o saldo atual da conta
         print(f"Seu saldo atual é: R${conta['saldo']:.2f}")
 
     def depositar(self, conta):
-        # Método para permitir que o usuário deposite dinheiro em sua conta
-        valor = float(input("Digite o valor a depositar: R$"))
+        valor_str = input("Digite o valor a depositar: R$").strip()
+
+        if not valor_str or not valor_str.isdigit():
+            print("Por favor, insira um valor válido.")
+            return
+
+        valor = float(valor_str)
+
         if valor > 0:
             conta['saldo'] += valor
             data_deposito = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -92,8 +101,14 @@ class ATM:
             print("Valor inválido.")
 
     def sacar(self, conta):
-        # Método para permitir que o usuário saque dinheiro de sua conta
-        valor = float(input("Digite o valor a sacar: R$"))
+        valor_str = input("Digite o valor a sacar: R$").strip()
+
+        if not valor_str or not valor_str.isdigit():
+            print("Por favor, insira um valor válido.")
+            return
+
+        valor = float(valor_str)
+
         if 0 < valor <= conta['saldo']:
             conta['saldo'] -= valor
             data_saque = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -103,9 +118,14 @@ class ATM:
             print("Saldo insuficiente ou valor inválido.")
 
     def transferir(self, conta_origem):
-        # Método para permitir que o usuário transfira dinheiro para outra conta
-        numero_destino = input("Digite o número da conta de destino: ")
-        valor = float(input("Digite o valor a transferir: R$"))
+        numero_destino = input("Digite o número da conta de destino: ").strip()
+        valor_str = input("Digite o valor a transferir: R$").strip()
+
+        if not numero_destino or not valor_str or not valor_str.isdigit():
+            print("Por favor, preencha todos os campos corretamente.")
+            return
+
+        valor = float(valor_str)
 
         for conta_destino in self.contas:
             if conta_destino['numero_conta'] == numero_destino:
@@ -113,14 +133,11 @@ class ATM:
                     conta_origem['saldo'] -= valor
                     conta_destino['saldo'] += valor
                     data_transferencia = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                    
-                    # Registro no histórico da conta de origem
+
                     nome_destino = conta_destino['nome']
                     conta_origem['historico'].append(f"Transferência de R${valor:.2f} para {nome_destino} ({conta_destino['numero_conta']}) em {data_transferencia}")
-                    
-                    # Registro no histórico da conta de destino
                     conta_destino['historico'].append(f"Transferência recebida de R${valor:.2f} de {conta_origem['nome']} ({conta_origem['numero_conta']}) em {data_transferencia}")
-                    
+
                     print(f"Transferência de R${valor:.2f} para {nome_destino} realizada com sucesso.")
                 else:
                     print("Saldo insuficiente ou valor inválido.")
@@ -129,7 +146,6 @@ class ATM:
         print("Conta de destino não encontrada.")
 
     def imprimir_relatorio(self, conta):
-        # Método para imprimir o relatório detalhado da conta, incluindo o histórico de transações
         print("\nRelatório da Conta:")
         print("Nome: {}".format(conta['nome']))
         print("Número da Conta: {}".format(conta['numero_conta']))
@@ -140,7 +156,6 @@ class ATM:
             print(registro)
 
     def executar(self):
-        # Método para iniciar a execução do programa
         while True:
             self.exibir_menu()
             escolha = input("Escolha uma opção (1-3): ")
@@ -156,6 +171,5 @@ class ATM:
                 print("Opção inválida. Tente novamente.")
 
 if __name__ == "__main__":
-    # Instanciar e executar o ATM
     atm = ATM()
     atm.executar()
